947cf08353c16911e792ffe7ba86087542261c17
Charles Gourdin <gourdin.charles@gmail.com>
2019-10-07 13:20:55 +0200

    Ajout de la Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Les Simpson</a>
+        </nav>
+    )
+}
+
+export default Navbar;
