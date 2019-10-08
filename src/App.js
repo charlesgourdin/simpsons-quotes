7207031ee1be19077969402b4957ef9c70feee60
Charles Gourdin <gourdin.charles@gmail.com>
2019-10-08 11:18:38 +0200

    fin quête 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
+      <QuoteCard character="Homer Simpson" quote="Facts are meaningless. You could use facts to prove anything that's even remotely true." image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939" />
+      <QuoteCard character="Waylon Smithers" quote="I think women and seamen don't mix." image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FWaylonSmithers.png?1497567511840" />   
     </div>
   );
 }
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './QuoteCard.css'
 
-const QuoteCard = () => {
+const QuoteCard = (props) => {
     return (
         <figure className="QuoteCard">
             <img
-            src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-            alt="Nelson Muntz"
+            src={props.image}
+            alt={props.character}
             />
             <figcaption>
                 <blockquote>
-                Shoplifting is a victimless crime, like punching someone in the dark.
+                {props.quote}
                 </blockquote>
-                <cite>Nelson Muntz</cite>
+                <cite>{props.character}</cite>
             </figcaption>
         </figure>
     );
